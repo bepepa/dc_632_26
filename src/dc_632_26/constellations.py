b2edf2c18099730c101156dd0a5773f7143f8dcf
hle40 <minhleng07@gmail.com>
2026-09-03 23:36:59 -0400

    Partially addressed Issue #15 - Constellation class is init with an array that specifies the symbol - The class provides support methods - __getitem__ implemented in the class - BPSK, QPSK, PSK8, and QAM16 were implemented TODO: pytest + docstring

--- src/dc_632_26/constellations.py
+++ src/dc_632_26/constellations.py
@@ -1,43 +1,114 @@
+from typing import TypeAlias
+
 import numpy as np
+import numpy.typing as npt
 
-BPSK = {
-    0b1: 1 + 0j,
-    0b0: -1 + 0j
-}
+NumpyInt: TypeAlias = np.int16 | np.int32 | np.int64
 
-QPSK = {
-    0b00:  (1+1j),
-    0b01:  (-1+1j),
-    0b11:  (-1-1j),
-    0b10:  (1-1j),
-}
 
-PSK8 = {
-    0b000:  (1+0j),
-    0b001:  np.sqrt(2)/2 * (1+1j),
-    0b011:  (0+1j),
-    0b010:  np.sqrt(2)/2 * (-1+1j),
-    0b110:  (-1+0j),
-    0b111:  np.sqrt(2)/2 * (-1-1j),
-    0b101:  (0-1j),
-    0b100:  np.sqrt(2)/2 * (1-1j),
-}
+class Constellation:
+    def __init__(self, mod_table: npt.NDArray[np.complex128]):
+        self.mod_table: npt.NDArray[np.complex128] = np.asarray(
+            mod_table, dtype=np.complex128
+        )
 
-QAM16 = {
-    0b0000: (-3 - 3j), # 0
-    0b0001: (-3 - 1j), # 1
-    0b0010: (-3 + 3j), # 2
-    0b0011: (-3 + 1j), # 3
-    0b0100: (-1 - 3j), # 4
-    0b0101: (-1 - 1j), # 5
-    0b0110: (-1 + 3j), # 6
-    0b0111: (-1 + 1j), # 7
-    0b1000: ( 3 - 3j), # 8
-    0b1001: ( 3 - 1j), # 9
-    0b1010: ( 3 + 3j), # 10
-    0b1011: ( 3 + 1j), # 11
-    0b1100: ( 1 - 3j), # 12
-    0b1101: ( 1 - 1j), # 13
-    0b1110: ( 1 + 3j), # 14
-    0b1111: ( 1 + 1j), # 15
-}
+    def get_symbol_energy(self) -> np.float64:
+        return np.mean(np.abs(self.mod_table) ** 2)
+
+    def get_number_bits_per_symbol(self) -> np.uint64:
+        return np.uint64(np.round(np.log2(len(self.mod_table))))
+
+    def get_bit_energy(self) -> np.float64:
+        return self.get_symbol_energy() / self.get_number_bits_per_symbol()
+
+    def get_min_distance(self) -> np.float64:
+        """
+        FIX ME: Summaryline.
+
+        Using Broadcasting feature in numpy arrays
+        https://numpy.org/doc/stable/user/basics.broadcasting.html
+
+        x = QPSK[:, np.newaxis]
+        array([[ 1.+1.j],
+               [-1.+1.j],
+               [-1.-1.j],
+               [ 1.-1.j]])
+
+        y = QPSK[np.newaxis, :]
+        array([[ 1.+1.j, -1.+1.j, -1.-1.j,  1.-1.j]])
+
+        x-y
+        array([[ 0.+0.j,  2.+0.j,  2.+2.j,  0.+2.j],
+               [-2.+0.j,  0.+0.j,  0.+2.j, -2.+2.j],
+               [-2.-2.j,  0.-2.j,  0.+0.j, -2.+0.j],
+               [ 0.-2.j,  2.-2.j,  2.+0.j,  0.+0.j]])
+        """
+        diff = np.abs(self.mod_table[:, np.newaxis] - self.mod_table[np.newaxis, :])
+        np.fill_diagonal(diff, np.inf)  # fill out the 0's
+        return diff.min()
+
+    def get_energy_efficiency(self) -> np.float64:
+        return self.get_min_distance() ** 2 / self.get_bit_energy()
+
+    def __getitem__(self, bit_seq: int | NumpyInt) -> np.complex128:
+        return self.mod_table[bit_seq]
+
+
+bpsk_table: npt.NDArray[np.complex128] = np.array(
+    [
+        1 + 0j,  # 0
+        -1 + 0j,  # 1
+    ],
+    dtype=np.complex128,
+)
+
+qpsk_table = np.array(
+    [
+        (1 + 1j),  # 0
+        (-1 + 1j),  # 1
+        (-1 - 1j),  # 2
+        (1 - 1j),  # 3
+    ],
+    dtype=np.complex128,
+)
+
+psk8_table = np.array(
+    [
+        (1 + 0j),  # 0
+        np.sqrt(2) / 2 * (1 + 1j),  # 1
+        (0 + 1j),  # 2
+        np.sqrt(2) / 2 * (-1 + 1j),  # 3
+        (-1 + 0j),  # 4
+        np.sqrt(2) / 2 * (-1 - 1j),  # 5
+        (0 - 1j),  # 6
+        np.sqrt(2) / 2 * (1 - 1j),  # 7
+    ],
+    dtype=np.complex128,
+)
+
+qam16_table = np.array(
+    [
+        (-3 - 3j),  # 0
+        (-3 - 1j),  # 1
+        (-3 + 3j),  # 2
+        (-3 + 1j),  # 3
+        (-1 - 3j),  # 4
+        (-1 - 1j),  # 5
+        (-1 + 3j),  # 6
+        (-1 + 1j),  # 7
+        (3 - 3j),  # 8
+        (3 - 1j),  # 9
+        (3 + 3j),  # 10
+        (3 + 1j),  # 11
+        (1 - 3j),  # 12
+        (1 - 1j),  # 13
+        (1 + 3j),  # 14
+        (1 + 1j),  # 15
+    ],
+    dtype=np.complex128,
+)
+
+BPSK = Constellation(bpsk_table)
+QPSK = Constellation(qpsk_table)
+PSK8 = Constellation(psk8_table)
+QAM16 = Constellation(qam16_table)
