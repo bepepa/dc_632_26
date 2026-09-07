bc36152f4204c24e78f6f05854650395686d76d9
Emily Zhu <zhuemily7@gmail.com>
2026-09-07 17:30:33 -0400

    cleanup

--- src/dc_632_26/pulse_shaper.py
+++ src/dc_632_26/pulse_shaper.py
@@ -4,7 +4,6 @@ import numpy as np
 class PulseShaper:
     """Class that generates a sampled waveform given pulse shape and input symbols.
 
-<<<<<<< HEAD
         Parameters
         ----------
         pulse : np.ndarray 
@@ -14,18 +13,6 @@ class PulseShaper:
     """ 
     def __init__(self, pulse: np.ndarray, oversamp: int):
         self.pulse = pulse
-=======
-    Parameters
-    ----------
-    pulse : function
-        Function that takes as an argument the number of points to return.
-    oversamp : float
-        Number of samples per symbol (sample rate divided by symbol rate)
-    """
-
-    def __init__(self, pulse, oversamp: float) -> np.ndarray:
-        self.pulse = pulse(oversamp)
->>>>>>> 40a6523b5854c3f4eb1a94de4d3a131d9f8a95a5
         self.oversamp = oversamp
 
     def generate_waveform(self, symbols: np.ndarray) -> np.ndarray:
@@ -46,15 +33,9 @@ class PulseShaper:
         >>> waveform = pulse_shaper.generate_waveform(symbols)
         """
         # Space the symbols out by the oversamp factor
-<<<<<<< HEAD
         waveform_len = (len(symbols)-1)*self.oversamp + 1
         spaced_symbols = np.zeros(waveform_len, dtype=complex) 
         spaced_symbols[::self.oversamp] = symbols
-=======
-        waveform_len = len(symbols) * self.oversamp
-        spaced_symbols = np.zeros(waveform_len, dtype=complex)
-        spaced_symbols[:: self.oversamp] = symbols
->>>>>>> 40a6523b5854c3f4eb1a94de4d3a131d9f8a95a5
 
         # Convolve with pulse to get waveform
         waveform = np.convolve(spaced_symbols, self.pulse)
