19290eea710e78bb85ecf32ec7282466b5e2a7fa
max kolsasch <mkolasch@gmu.edu>
2026-09-07 21:00:11 -0400

    test: expand constellations support functions for Eb, dmin, eta, and __getitem__

--- test/test_constellations.py
+++ test/test_constellations.py
@@ -1,21 +1,40 @@
+"""
+Completes Bulding tests for the support functions
+developed within constellations.py constellations class. 
+"""
+
 import numpy as np
 import pytest
 
 from dc_632_26.constellations import BPSK, PSK8, QAM16, QPSK, Constellation
 
+"""
+__getitem__ loops over every point of all four constellations.
+"""
+@pytest.mark.parametrize(
+    "constellation, expected_points",
+    [
+        (BPSK, [1 + 0j, -1 + 0j]),
+        (QPSK, [1 + 1j, -1 + 1j, -1 - 1j, 1 - 1j]),
+        (PSK8, [1 + 0j, np.sqrt(2) / 2 * (1 + 1j),
+                0 + 1j, np.sqrt(2) / 2 * (-1 + 1j),
+                -1 + 0j, np.sqrt(2) / 2 * (-1 - 1j),
+                0 - 1j, np.sqrt(2) / 2 * (1 - 1j)]),
+        (QAM16, [-3 - 3j, -3 - 1j, -3 + 3j, -3 + 1j,
+                 -1 - 3j, -1 - 1j, -1 + 3j, -1 + 1j,
+                 3 - 3j, 3 - 1j, 3 + 3j, 3 + 1j,
+                 1 - 3j, 1 - 1j, 1 + 3j, 1 + 1j])
+    ]
+)
 
 class TestGetItem:
     """Tests __getitem__."""
 
-    def test_bpsk_indexing(self):
-        """Tests __getitem__ BPSK."""
-        assert BPSK[0] == pytest.approx(1 + 0j)
-        assert BPSK[1] == pytest.approx(-1 + 0j)
-
-    def test_qpsk_indexing(self):
-        """Tests __getitem__ BPSK."""
-        assert QPSK[0] == pytest.approx(1 + 1j)
-        assert QPSK[3] == pytest.approx(1 - 1j)
+    def test_get_item(self, constellation, expected_points):
+        """returns expected constellation point for every index i"""
+        for i, expected in enumerate(expected_points):
+            assert constellation[i] == pytest.approx(expected)
+            assert isinstance(constellation[i], complex)
 
 
 @pytest.mark.parametrize(
@@ -27,6 +46,7 @@ class TestGetItem:
         (QAM16, 4),
     ],
 )
+
 class TestNumberOfBitsPerSymbol:
     """Tests for getting the number of bits in predefine constellations."""
 
@@ -45,6 +65,7 @@ class TestNumberOfBitsPerSymbol:
         (QAM16, 10),
     ],
 )
+
 class TestSymbolEnregy:
     """Tests for get_symbol_energy."""
 
@@ -52,3 +73,60 @@ class TestSymbolEnregy:
         """Run the same test for all parameterized inputs."""
         assert constellation.symbol_energy == pytest.approx(expected_symbol_energy)
         assert constellation.Es == pytest.approx(expected_symbol_energy)
+
+
+@pytest.mark.parametrize(
+    "constellation, expected_bit_energy",
+    [
+        (BPSK, 1.0),
+        (QPSK, 1.0),
+        (PSK8, 1/3),
+        (QAM16, 2.5),
+    ],
+)
+
+class TestBitEnergy:
+    """Tests for get_bit_energy."""
+
+    def test_bit_energy(self, constellation, expected_bit_energy):
+        """Run the same test for all parametrized inputs."""
+        assert constellation.bit_energy == pytest.approx(expected_bit_energy)
+        assert constellation.Eb == pytest.approx(expected_bit_energy)
+
+
+@pytest.mark.parametrize(
+    "constellation, expected_min_distance",
+    [
+        (BPSK, 2),
+        (QPSK, 2),
+        (PSK8, 2*np.sin(np.pi/8)),
+        (QAM16, 2),
+    ],
+)
+
+class TestMinDistance:
+    """Tests for get_min_distance."""
+
+    def test_min_distance(self, constellation, expected_min_distance):
+        """Run the same test for all parametrized inputs"""
+        assert constellation.min_distance == pytest.approx(expected_min_distance)
+        assert constellation.dmin == pytest.approx(expected_min_distance)
+
+    
+@pytest.mark.parametrize(
+    "constellation, expected_energy_efficiency",
+    [
+        (BPSK, 4),
+        (QPSK, 4),
+        (PSK8, (2 * np.sin(np.pi / 8)) ** 2 / (1 / 3)),
+        (QAM16, 1.6),
+    ],
+)
+
+class TestEnergyEfficiency:
+    """Tests for get_energy_efficiency."""
+
+    def test_energy_efficiency(self, constellation, expected_energy_efficiency):
+        """Run the same test for all parametrized inputs"""
+        assert constellation.energy_efficiency == pytest.approx(expected_energy_efficiency)
+        assert constellation.eta == pytest.approx(expected_energy_efficiency)
