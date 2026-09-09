683ce343d3f62e00f92c9b7b28504d3c562e36dc
Emily Zhu <zhuemily7@gmail.com>
2026-09-08 22:12:03 -0400

    add test for pulse length

--- test/test_pulses.py
+++ test/test_pulses.py
@@ -12,7 +12,14 @@ def test_unit_energy():
         triangular_pulse = pulses.TrianglePulse(num_samples)
         cosine_squared_pulse = pulses.CosineSquaredPulse(num_samples)
         half_sine_pulse = pulses.HalfSinePulse(num_samples)
-        
+
+        # Test pulse length
+        assert len(rectangular_pulse.samples) == num_samples
+        assert len(triangular_pulse.samples) == num_samples
+        assert len(cosine_squared_pulse.samples) == num_samples
+        assert len(half_sine_pulse.samples) == num_samples
+
+        # Test unit energy
         np.testing.assert_almost_equal(np.sum(rectangular_pulse.samples ** 2), 1, 8)
         np.testing.assert_almost_equal(np.sum(triangular_pulse.samples ** 2), 1, 8)
         np.testing.assert_almost_equal(np.sum(cosine_squared_pulse.samples ** 2), 1, 8)
