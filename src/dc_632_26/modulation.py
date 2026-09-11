42df7c30c2998917b0376455e2dd975b68ae0076
culrich3 <culrich3@gmu.edu>
2026-09-10 22:37:49 -0400

    Vectorized bits_to_int, changed testing, addressed some comments

--- src/dc_632_26/modulation.py
+++ src/dc_632_26/modulation.py
@@ -1,60 +1,66 @@
 import numpy as np
 import numpy.typing as npt
-from itertools import batched
 
-def bits_to_int(bits: npt.ArrayLike) -> int:
+def bits_to_int( bits_BS: npt.ArrayLike ) -> np.ndarray:
     """
     Function that takes a list of bit represented by integers
     and outputs an integer.
 
     Parameters
     ----------
-    bits : npt.ArrayLike
+    bits_BS : npt.ArrayLike
         bitstring represented as zeros and ones.
-
+        Shaped to process multiple symbols at once.
+        Number of rows is number of bits per symbol. MSB is first(top) - B
+        Number of columns is number of symbols. - S
     Returns
     -------
-    int
+    np.ndarray
         The bits converted to an integer
     """
 
-    result = 0
-    bits = np.asarray(bits)
+    bits_BS = np.asarray( bits_BS )
+    result_S = np.zeros( bits_BS.shape[-1] ).astype( 'int' )
     # MSB to LSB
-    for bit in bits:
-        result = result << 1
-        result += bit
-    return result
+    for bit in bits_BS:
+        result_S = result_S << 1
+        result_S += bit
+    return result_S
 
-def modulation(bits: np.ndarray, constellation: dict[int, np.complex128]) -> np.ndarray:
+def modulation( bits_N: np.ndarray, constellation_map_O: np.ndarray ) -> np.ndarray:
     """
     Function that takes bits and maps them according to the mapper.
     The bits are a numpy ndarray, the mapper is a function that should
-    take the bits two at a time and map them to a complex value.
+    take the bits bits_per_symbol at a time and map them to a complex value.
 
     Parameters
     ----------
-    bits : np.ndarray
+    bits_N : np.ndarray
         bitstring represented as zeros and ones
-    constellation : dict[int, np.complex128]
-        mapping between values of the bits, in integer form,
+        N is the number of bits, it can change slightly
+        if we pad out the sequence.
+    constellation_map_O : np.ndarray
+        mapping between values of the bits, as indices,
         and the actual complex value of the symbol sequence at
         that point.
+        O is the modulation order.
 
     Returns
     -------
     np.ndarray
         The complex symbols corresponding to the given
         bitstring
+        S is the number of symbols
+        B is the number of bits per symbol
     """
 
-    bits_per_symbol = int(np.log2(len(constellation)))
-    remainder = bits.size % bits_per_symbol
+    bits_per_symbol = int( np.log2( len( constellation_map_O ) ) )
+    remainder = bits_N.size % bits_per_symbol
     if remainder != 0:
         padding = bits_per_symbol - remainder
-        bits = np.append(bits, [0]*padding).astype('int')
-    symbols = np.empty(bits.size//bits_per_symbol, dtype=np.complex128)
-    for idx, bit_seq in enumerate(batched(bits, bits_per_symbol)):
-       symbols[idx] = constellation[bits_to_int(bit_seq)]
+        bits_N = np.append( bits_N, [0] * padding).astype('int')
+    symbols_S = np.empty( bits_N.size // bits_per_symbol, dtype=np.complex128)
+    bits_reshaped_BS = np.reshape( bits_N, (bits_per_symbol, -1), order='F')
+    symbols_S = constellation_map_O[ bits_to_int( bits_reshaped_BS ) ]
 
-    return symbols
+    return symbols_S
--- test/test_modulation.py
+++ test/test_modulation.py
@@ -2,12 +2,14 @@ import numpy as np
 import numpy.testing as test
 from dc_632_26.modulation import *
 
-TEST_QPSK = {
-    0: 1+1j,
-    1: -1+1j,
-    3: -1-1j,
-    2: 1-1j
-}
+# Keeping this for reference
+# TEST_QPSK = {
+#     0: 1+1j,
+#     1: -1+1j,
+#     3: -1-1j,
+#     2: 1-1j
+# }
+TEST_QPSK = np.array([1+1j,-1+1j,1-1j,-1-1j])
 
 def test_modulation():
     # Test base case of no remainder
@@ -21,15 +23,14 @@ def test_modulation():
     test.assert_array_equal(symbols,np.array([-1+1j,-1-1j,-1+1j,1-1j]))
 
 def test_bits_to_int():
-    # 0
     bits = np.array([0])
     result = bits_to_int(bits)
     test.assert_equal(result, 0)
-    # 111
-    bits = np.array([1,1,1])
+
+    bits = np.array([[1,1,1],[1,1,1],[0,1,1]])
     result = bits_to_int(bits)
-    test.assert_equal(result, 7)
-    # 0110
-    bits = np.array([0,1,1,0])
+    test.assert_array_equal(result, np.array([6,7,7]))
+
+    bits = np.array([[0,1],[1,0],[1,1]])
     result = bits_to_int(bits)
-    test.assert_equal(result, 6)
+    test.assert_equal(result, np.array([3,5]))
