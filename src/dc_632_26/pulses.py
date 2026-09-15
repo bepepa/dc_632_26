c803b55441099b1635c777802952cf379d819dca
Peter Paris <pparis@gmu.edu>
2026-09-15 13:45:48 -0400

    added computation of frequency response for rectangular pulse

--- src/dc_632_26/pulses.py
+++ src/dc_632_26/pulses.py
@@ -1,23 +1,27 @@
 import numpy as np
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+from typing import Tuple
 from abc import ABC, abstractmethod
+
 
 class Pulse(ABC):
     """Base class representing a pulse
-        Parameters
-        ----------
-        oversamp : int
-            Oversampling rate / number of samples in one pulse
-        nfft : int, optional
-            Number of FFT points to use when calculating frequency response. Default is 4096.
-    """ 
+    Parameters
+    ----------
+    oversamp : int
+        Oversampling rate / number of samples in one pulse
+    nfft : int, optional
+        Number of FFT points to use when calculating frequency response. Default is 4096.
+    """
+
     def __init__(self, num_samples, nfft=4096):
         self.num_samples = num_samples
+        self.oversamp = num_samples  ## FIX ME: needs to be separate from num_samples
         self.samples = self._generate_samples()
         self.nfft = nfft
 
     @abstractmethod
-    def _generate_samples(self):
+    def _generate_samples(self) -> np.ndarray:
         """Subclasses must implement.
         Generates the discrete-time pulse samples."""
         pass
@@ -38,8 +42,21 @@ class Pulse(ABC):
         energy = np.sum(samples**2)
         return samples / np.sqrt(energy)
 
-    def analytic_freq_response(self):
-        """Optional method to retun the analytic frequency response of the pulse, starting at freq 0. 
+    def analytic_freq_response(self, T: float = 1.0) -> Tuple[np.ndarray, np.ndarray]:
+        """Optional method to return the analytic frequency response of the pulse.
+
+        Parameters
+        ----------
+        T : float
+            Duration of the pulse in seconds. Default is 1.0.
+
+        Returns
+        -------
+        Tuple[np.ndarray, np.ndarray]
+            Frequencies and corresponding frequency response values
+            The frequency response is calculated analytically by the subclass, if implemented.
+        Notes
+        -----
         Implemented by subclass.
         """
         raise NotImplementedError("Not yet implemented by subclass")
@@ -53,7 +70,7 @@ class Pulse(ABC):
             Discrete Fourier Transform of the pulse
         """
         return np.fft.fft(self.samples, n=self.nfft)
-    
+
     def plot_freq_response(self, fs: float):
         """Creates a plot of the pulse's frequency response (magnitude only)
 
@@ -67,13 +84,13 @@ class Pulse(ABC):
         # Get frequency response
         H = np.fft.fftshift(self.freq_response())
         # Frequency axis in Hz
-        freqs = np.fft.fftshift(np.fft.fftfreq(self.nfft, d=1/fs))
+        freqs = np.fft.fftshift(np.fft.fftfreq(self.nfft, d=1 / fs))
 
-        #Plot magnitude
-        plt.plot(freqs, np.abs(H), label='DFT')
-        try: # Plot the analytic response, if available
-            H_analytic = self.analytic_freq_response()
-            plt.plot(freqs, np.abs(H_analytic), label='Analytic')
+        # Plot magnitude
+        plt.plot(freqs, np.abs(H), label="DFT")
+        try:  # Plot the analytic response, if available
+            H_analytic_freqs, H_analytic = self.analytic_freq_response()
+            plt.plot(H_analytic_freqs, np.abs(H_analytic), label="Analytic")
             plt.legend()
         except NotImplementedError:
             pass
@@ -99,30 +116,52 @@ class RectangularPulse(Pulse):
         samples = np.ones(self.num_samples)
         return self._normalize_energy(samples)
 
+    def analytic_freq_response(self, T: float = 1.0) -> Tuple[np.ndarray, np.ndarray]:
+        """Optional method to return the analytic frequency response of the pulse.
+
+        Parameters
+        ----------
+        T : float
+            Duration of the pulse in seconds. Default is 1.0.
+
+        Returns
+        -------
+        Tuple[np.ndarray, np.ndarray]
+            Frequencies and corresponding frequency response values
+            The frequency response is calculated analytically by the subclass, if implemented.
+        """
+        freqs = np.linspace(
+            -self.oversamp / (2 * T), self.oversamp / (2 * T), self.nfft
+        )
+        H = np.sqrt(T) * np.sinc(freqs * T)
+        return freqs, H
+
 
 class HalfSinePulse(Pulse):
     """Half-sine pulse
 
-        Parameters
-            ----------
-            num_samples : int
-                Number of samples in the pulse
-        """
-    
+    Parameters
+        ----------
+        num_samples : int
+            Number of samples in the pulse
+    """
+
     def __init__(self, num_samples):
         super().__init__(num_samples)
 
     def _generate_samples(self):
-        return self._normalize_energy(np.sin(np.pi * np.linspace(0., 1., self.num_samples, endpoint=False)))
+        return self._normalize_energy(
+            np.sin(np.pi * np.linspace(0.0, 1.0, self.num_samples, endpoint=False))
+        )
 
-    
+
 class CosineSquaredPulse(Pulse):
     """Cosine-squared pulse
 
-        Parameters
-            ----------
-            num_samples : int
-                Number of samples in the pulse
+    Parameters
+        ----------
+        num_samples : int
+            Number of samples in the pulse
     """
 
     def __init__(self, num_samples):
@@ -131,10 +170,10 @@ class CosineSquaredPulse(Pulse):
     def _generate_samples(self):
         # Time axis
         t = np.arange(self.num_samples) / self.num_samples
-        
+
         # Creating the cosine squared shape
         pulse = 1 / 2 - 1 / 2 * np.cos(2 * np.pi * t)
-        
+
         # Normalizing to unit energy
         return self._normalize_energy(pulse)
 
@@ -142,10 +181,10 @@ class CosineSquaredPulse(Pulse):
 class TrianglePulse(Pulse):
     """Triangular pulse
 
-        Parameters
-            ----------
-            num_samples : int
-                Number of samples in the pulse
+    Parameters
+        ----------
+        num_samples : int
+            Number of samples in the pulse
     """
 
     def __init__(self, num_samples):
@@ -154,13 +193,13 @@ class TrianglePulse(Pulse):
     def _generate_samples(self):
         # Time axis
         t = np.arange(self.num_samples) / self.num_samples
-        
+
         # Creating the triangular shape
-        pulse = np.piecewise(t,
-                            [((0 <= t) & (t < 0.5)),
-                            ((0.5 <= t) & (t < 1))],
-                            [lambda t: t,
-                            lambda t: 1 - t])
-        
+        pulse = np.piecewise(
+            t,
+            [((0 <= t) & (t < 0.5)), ((0.5 <= t) & (t < 1))],
+            [lambda t: t, lambda t: 1 - t],
+        )
+
         # Normalizing to unit energy
         return self._normalize_energy(pulse)
